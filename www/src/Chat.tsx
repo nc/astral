f2f8d313f1352cb1c71b8a6a45abce9f4209fbaf
Namit Chadha <namit.chadha@gmail.com>
2025-10-04 19:33:15 +0100

    styling

--- www/src/Chat.tsx
+++ www/src/Chat.tsx
@@ -853,7 +853,7 @@ export function Chat({ chat }: ChatProps) {
             <div style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}>
               <div
                 style={{
-                  maxWidth: 720,
+                  maxWidth: 744,
                   width: "100%",
                   display: "table",
                   margin: "0 auto",
